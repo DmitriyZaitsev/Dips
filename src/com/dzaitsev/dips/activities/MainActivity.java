7074e4a10faba6f6e08a95b1f79aee05186983ae
Dmitriy Zaitsev <d.zaitsev@samsung.com>
2013-04-30 16:59:41 +0300

    extracted some abstractions and performed a bit incapsulation

--- src/com/dzaitsev/dips/activities/MainActivity.java
+++ src/com/dzaitsev/dips/activities/MainActivity.java
@@ -12,23 +12,22 @@ import com.dzaitsev.dips.DipsPreferences;
 import com.dzaitsev.dips.IDipsPreferences;
 import com.dzaitsev.dips.R;
 import com.dzaitsev.dips.exercises.Dips;
+import com.dzaitsev.dips.exercises.Exercise;
 
 public class MainActivity extends Activity {
 	private final int CODE_REQUEST = 1;
 	private Button mDoneButton;
-	private Dips mDips;
+	private Exercise mDips;
 	private IDipsPreferences mPrefs;
-	private TextView mCurrentSetTextView;
-	private TextView mDoneTextView;
+	private TextView mCurrentTextView;
+	private TextView mCompletedTextView;
 	private TextView mRemainingTextView;
-	private int mExercisesDone;
-	private int mExercisesRemaining;
 	private int mUserLevel;
 	
 	@Override protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		if (resultCode == Activity.RESULT_OK) {
 			updateUserProgress();
-			if (mExercisesRemaining <= 0) {
+			if (mDips.getRemaining() <= 0) {
 				mDoneButton.setText(R.string.finish);
 			}
 		} else if (resultCode == Activity.RESULT_CANCELED) {
@@ -44,17 +43,17 @@ public class MainActivity extends Activity {
 		initUserProgress();
 
 		mDoneButton = (Button) findViewById(R.id.btn_done);
-		mCurrentSetTextView = (TextView) findViewById(R.id.tv_cur_exercises);
-		mDoneTextView = (TextView) findViewById(R.id.tv_done);
+		mCurrentTextView = (TextView) findViewById(R.id.tv_cur_exercises);
+		mCompletedTextView = (TextView) findViewById(R.id.tv_done);
 		mRemainingTextView = (TextView) findViewById(R.id.tv_remaining);
 
-		mCurrentSetTextView.setText(String.valueOf(mDips.getCurrentDips()));
-		mDoneTextView.setText("0");
-		mRemainingTextView.setText(String.valueOf(mExercisesRemaining));
+		mCurrentTextView.setText(String.valueOf(mDips.getCurrent()));
+		mCompletedTextView.setText("0");
+		mRemainingTextView.setText(String.valueOf(mDips.getRemaining()));
 
 		mDoneButton.setOnClickListener(new View.OnClickListener() {
 			@Override public void onClick(final View view) {
-				if (mExercisesRemaining <= 0) {
+				if (mDips.getRemaining() <= 0) {
 					showAlertDialog();
 				} else {
 					Intent intent = new Intent(MainActivity.this, TimerActivity.class);
@@ -67,9 +66,6 @@ public class MainActivity extends Activity {
 	private void initUserProgress() {
 		mUserLevel = mPrefs.getUserLevel();
 		mDips = new Dips(mUserLevel);
-
-		mExercisesRemaining = mDips.getSet1() + mDips.getSet2() + mDips.getSet3() + mDips
-				.getSet4() + mDips.getSet5();
 	}
 
 	private void showAlertDialog() {
@@ -86,12 +82,10 @@ public class MainActivity extends Activity {
 
 	private void updateUserProgress() {
 		//TODO: Update user progress
-		mExercisesDone += mDips.getCurrentDips();
-		mExercisesRemaining -= mDips.getCurrentDips();
 		mDips.confirmSet();
 
-		mCurrentSetTextView.setText(String.valueOf(mDips.getCurrentDips()));
-		mDoneTextView.setText(String.valueOf(mExercisesDone));
-		mRemainingTextView.setText(String.valueOf(mExercisesRemaining));
+		mCurrentTextView.setText(String.valueOf(mDips.getCurrent()));
+		mCompletedTextView.setText(String.valueOf(mDips.getCompleted()));
+		mRemainingTextView.setText(String.valueOf(mDips.getRemaining()));
 	}
 }
--- src/com/dzaitsev/dips/exercises/Exercise.java
+++ src/com/dzaitsev/dips/exercises/Exercise.java
@@ -6,20 +6,85 @@ package com.dzaitsev.dips.exercises;
  * Created by Dmitriy Zaitsev at 2013-04-29, 17:47.<br>
  */
 public abstract class Exercise {
-	public static final int MAX_LEVEL = 100;
-	public static final int MAX_SET = 100;
-	public static final int MIN_LEVEL = 1;
-	public static final int MIN_SET = 1;
+	protected int mMaxLevel;
+	protected int mMaxSet;
+	protected static final int MIN_LEVEL = 1;
+	protected static final int MIN_SET = 1;
+	protected int mCompletedExercises;
 	protected int mCurrentExercises;
 	protected int mCurrentSet;
 	protected int mLevel;
+	
+	public Exercise(final int level) {
+		reset();
+	}
+	
+  /**
+	* @param number number of set
+	*
+	* @return amount of exercises of set
+	*/
+	public abstract int getSet(final int number);
 
-	/** @return number of total com.dzaitsev.dips.exercises amount for all sets */
-	public abstract int getTotalExercises();
+	/** @return number of completed exercises */
+	public int getCompleted() {
+		return mCompletedExercises;
+	}
+	
+	/** @return number of current dips necessary to do */
+	public int getCurrent() {
+		return mCurrentExercises;
+	}
+
+	/** @return number of current set */
+	public int getCurrentSet() {
+		if (mCurrentSet < MIN_SET) {
+			return MIN_SET;
+		} else if (mCurrentSet > mMaxSet) {
+			return mMaxSet;
+		} else {
+			return mCurrentSet;
+		}
+	}
+
+	public int getRemaining() {
+		return getTotal() - mCompletedExercises;
+	}
+
+	/** @return number of total exercises amount for all sets */
+	 public int getTotal() {
+		int total = 0;
+
+		for (int i = MIN_SET; i <= mMaxSet; i++) {
+			total += getSet(i);
+		}
+		return total;
+	}
 
 	/** Confirm that current set was successfully finished */
-	public abstract void confirmSet();
+	public void confirmSet() {
+		mCompletedExercises += mCurrentExercises;
+		if (mCompletedExercises > getTotal()) {
+			mCompletedExercises = getTotal();
+		}
+		
+		mCurrentSet++;
+		
+		if (mCurrentSet < MIN_SET) {
+			mCurrentSet = MIN_SET;
+		} else if (mCurrentSet > mMaxSet) {
+			mCurrentSet = mMaxSet;
+			mCurrentExercises = 0;
+			return;
+		}
+
+		mCurrentExercises = getSet(mCurrentSet);
+	}
 
 	/** Sets values of current exercise to default */
-	public abstract void reset();
+	public void reset() {
+		mCurrentSet = MIN_SET;
+		mCurrentExercises = getSet(1);
+		mCompletedExercises = 0;
+	}
 }
